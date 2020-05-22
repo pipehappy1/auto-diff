41b0e02e7383f447f143a7d0663a04b337a10ef6
yguan <yguan@example.com>
2020-05-22 23:50:53 +0800

    test conv2d over mnist

--- examples/data/download.sh
+++ examples/data/download.sh
@@ -1,1 +1,8 @@
 wget http://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wdbc.data -P examples/data/
+
+mkdir -p data/mnist
+curl http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz --output "./data/mnist/train-images-idx3-ubyte.gz"
+curl http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz --output "./data/mnist/train-labels-idx1-ubyte.gz"
+curl http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz --output "./data/mnist/t10k-images-idx3-ubyte.gz"
+curl http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz --output "./data/mnist/t10k-labels-idx1-ubyte.gz"
+gzip -d data/mnist/*.gz
---
+++ examples/mnist.rs
@@ -0,0 +1,13 @@
+use std::fs::File;
+use std::path::Path;
+use std::io;
+use std::io::Read;
+
+use auto_diff::tensor::Tensor;
+
+fn load_images() {
+    
+}
+
+#[allow(dead_code)]
+fn main() {}    
