10fc7d6a839ba100b057886190d5d92a0bc10d08
yguan <yguan@example.com>
2022-01-15 00:38:56 +0800

    add test for lu decomp

--- tensor-rs/src/tensor/linalg.rs
+++ tensor-rs/src/tensor/linalg.rs
@@ -5,7 +5,7 @@ pub trait ElemwiseTensorOp {
     type ElementType;
 
     fn lu(&self) -> Option<[Self::TensorType; 2]>;
-    fn det(&self) -> Self::ElementType;
+    fn det(&self) -> Option<Self::ElementType>;
 }
 
 
@@ -40,8 +40,14 @@ where T: num_traits::Float {
         Some([l, u])
     }
     
-    fn det(&self) -> Self::ElementType {
-        T::zero()
+    fn det(&self) -> Option<Self::ElementType> {
+        if let Some(v) = self.lu() {
+            let [l, u] = v;
+
+            Some(T::zero())
+        } else {
+            None
+        }
     }
 }
 
@@ -53,8 +59,10 @@ mod tests {
     #[test]
     fn lu() {
         let m = GenTensor::<f64>::new_raw(&[1., 1., 1., 4., 3., -1., 3., 5., 3.], &[3,3]);
-        println!("{:?}", m);
         let [l, u] = m.lu().unwrap();
-        println!("{:?}, {:?}", l, u);
+        let el = GenTensor::<f64>::new_raw(&[1., 0., 0., 4., 1., 0., 3., -2., 1.], &[3,3]);
+        let eu = GenTensor::<f64>::new_raw(&[1., 1., 1., 0., -1., -5., 0., 0., -10.], &[3,3]);
+        assert_eq!(l, el);
+        assert_eq!(u, eu);
     }
 }
