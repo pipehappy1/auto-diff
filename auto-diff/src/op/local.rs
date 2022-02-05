48ce19a64217e635ab5930f145eb52914c9b8d3c
yguan <yguan@example.com>
2022-02-05 10:55:25 +0800

    share macros

--- auto-diff/src/op/local.rs
+++ auto-diff/src/op/local.rs
@@ -1,53 +1,8 @@
 #![allow(clippy::redundant_closure_call)]
 use tensor_rs::tensor::Tensor;
 use super::{OpTrait, OpHandle};
+use super::macros::new_binary_op;
 
-macro_rules! new_binary_op {
-    ($a:ident, $b:expr, $c:tt, $d: tt) => {
-        pub struct $a {
-            handle: OpHandle,
-        }
-        impl $a {
-            pub fn new() -> $a{
-                $a{
-                    handle: OpHandle::new(),
-                }
-            }
-            fn get_handle(&self) -> &OpHandle {
-                &self.handle
-            }
-            fn get_handle_mut(&mut self) -> &mut OpHandle {
-                &mut self.handle
-            }
-        }
-        impl OpTrait for $a {
-     
-            fn get_name(&self) -> String {
-                ($b).to_string()
-            }
-            fn get_input_size(&self) -> usize {
-                2
-            }
-            fn get_output_size(&self) -> usize {
-                1
-            }
-            fn apply(&self, input: &[Tensor], output: &[Tensor]) {
-                $c(input, output)
-            }
-            fn grad(&self, input: &[Tensor], output_grad: &[Tensor], input_grad: &[Tensor]) {
-                $d(input, output_grad, input_grad)
-            }
-            fn get_values(&self) -> Vec<Tensor> {
-                Vec::new()
-            }
-            fn get_grads(&self) -> Vec<Tensor> {
-                Vec::new()
-            }
-            fn set_values(&self, _v: &[Tensor]) {
-            }
-        }
-    }
-}
 
 new_binary_op!(Add, "add",
                (|a:&[Tensor], b:&[Tensor]|
--- auto-diff/src/op/macros.rs
+++ auto-diff/src/op/macros.rs
@@ -61,4 +61,101 @@ macro_rules! many_to_1_op_with_paras {
         }
     }
 }
+
+macro_rules! new_binary_op {
+    ($a:ident, $b:expr, $c:tt, $d: tt) => {
+        pub struct $a {
+            handle: OpHandle,
+        }
+        impl $a {
+            pub fn new() -> $a{
+                $a{
+                    handle: OpHandle::new(),
+                }
+            }
+            fn get_handle(&self) -> &OpHandle {
+                &self.handle
+            }
+            fn get_handle_mut(&mut self) -> &mut OpHandle {
+                &mut self.handle
+            }
+        }
+        impl OpTrait for $a {
+     
+            fn get_name(&self) -> String {
+                ($b).to_string()
+            }
+            fn get_input_size(&self) -> usize {
+                2
+            }
+            fn get_output_size(&self) -> usize {
+                1
+            }
+            fn apply(&self, input: &[Tensor], output: &[Tensor]) {
+                $c(input, output)
+            }
+            fn grad(&self, input: &[Tensor], output_grad: &[Tensor], input_grad: &[Tensor]) {
+                $d(input, output_grad, input_grad)
+            }
+            fn get_values(&self) -> Vec<Tensor> {
+                Vec::new()
+            }
+            fn get_grads(&self) -> Vec<Tensor> {
+                Vec::new()
+            }
+            fn set_values(&self, _v: &[Tensor]) {
+            }
+        }
+    }
+}
+
+macro_rules! new_element_op {
+    ($a:ident, $b:expr, $c:ident, $d: tt) => {
+        pub struct $a {
+            handle: OpHandle,
+        }
+        impl $a {
+            pub fn new() -> $a{
+                $a{
+                    handle: OpHandle::new(),
+                }
+            }
+            fn get_handle(&self) -> &OpHandle {
+                &self.handle
+            }
+            fn get_handle_mut(&mut self) -> &mut OpHandle {
+                &mut self.handle
+            }
+        }
+        impl OpTrait for $a {
+     
+            fn get_name(&self) -> String {
+                ($b).to_string()
+            }
+            fn get_input_size(&self) -> usize {
+                2
+            }
+            fn get_output_size(&self) -> usize {
+                1
+            }
+            fn apply(&self, input: &[Tensor], output: &[Tensor]) {
+                output[0].swap(&input[0].$c())
+            }
+            fn grad(&self, input: &[Tensor], output_grad: &[Tensor], input_grad: &[Tensor]) {
+                $d(input, output_grad, input_grad)
+            }
+            fn get_values(&self) -> Vec<Tensor> {
+                Vec::new()
+            }
+            fn get_grads(&self) -> Vec<Tensor> {
+                Vec::new()
+            }
+            fn set_values(&self, _v: &[Tensor]) {
+            }
+        }
+    }
+}
+
 pub(crate) use many_to_1_op_with_paras;
+pub(crate) use new_binary_op;
+pub(crate) use new_element_op;
