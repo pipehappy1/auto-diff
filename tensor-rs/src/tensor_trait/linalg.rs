f86591a6be96985d8cb0e4e7960cf5de44cb48ed
yguan <yguan@example.com>
2022-02-21 10:48:04 +0800

    bump versio to v0.5.4

--- tensor-rs/src/tensor_trait/linalg.rs
+++ tensor-rs/src/tensor_trait/linalg.rs
@@ -3,6 +3,8 @@ pub trait LinearAlgbra {
     type ElementType;
 
     fn norm(&self) -> Self::TensorType;
+    /// Assuming the input is 2 dimensional array,
+    /// normalize_unit 
     fn normalize_unit(&self) -> Self::TensorType;
     fn lu(&self) -> Option<[Self::TensorType; 2]>;
     fn lu_solve(&self, y: &Self::TensorType) -> Option<Self::TensorType>;
